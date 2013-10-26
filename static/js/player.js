a421f036e0d2c5104e308d227bd28ac612f34cdc
Oleg Elifantiev <oleg@elifantiev.ru>
2013-10-26 19:09:34 +0400

    Player object prototype

---
+++ static/js/player.js
@@ -0,0 +1,49 @@
+function Player(id, name, hp, fireCallback) {
+
+    this._id = id;
+    this._name = name;
+    this._hp = hp;
+    this._fireCallback = fireCallback;
+
+    this._render();
+}
+
+Player.prototype._render = function() {
+
+    // TODO(sasha-vinogradov) render name
+    // TODO(sasha-vinogradov) render hp indicator
+
+    // TODO(igor-nikitin) setup fire listener
+    /*
+     ??? Как узнать на какой DOM элемент подвешивать события ???
+        var self = this;
+        $('body').on('mousedown', function() { ... });
+        $('body').on('mouseup', function() {
+           ...
+           ...
+           ...
+
+            self._fireCallback(self._id, x, y);
+        });
+     */
+};
+
+Player.prototype.setHP = function(hp) {
+    // TODO(sasha-vinogradov) change hp value
+    // TODO(sasha-vinogradov) update hp indicator on screen
+};
+
+
+Player.prototype.getHP = function() {
+    return this._hp;
+};
+
+Player.prototype.getName = function() {
+   return this._name;
+};
+
+
+
+new Player(1, 'vasya', 3, function(id, x, y){
+    console.log(x + ' ' + y);
+});
